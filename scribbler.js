a37449e267d1bff368f9a6e3a956d7ecd57c82b4
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-03-19 22:20:18 +0530

    update
    update

--- scribbler.js
+++ scribbler.js
@@ -12,16 +12,8 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
-
-            ###todo for new year dinner party
-
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
-  var speed = 60;
+  var txt = `This project utilizes machine learning to predict multiple diseases from patient data, offering a sophisticated prognosis system. It integrates diverse data sources and employs Logistic Regression and SVM algorithms. With a focus on data quality and a user-friendly interface, it aims to revolutionize healthcare outcomes and preventive care strategies.`;
+  var speed = 10;
 
   function typeItOut () {
     if (i < txt.length) {
